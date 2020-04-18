cfec38893919a75d250212739a6b5719994b15c6
Florian Delage <delageflo@ymail.com>
2020-04-18 22:07:14 +0200

    create script creation tables

--- db_creation.py
+++ db_creation.py
@@ -13,11 +13,13 @@ mycursor = db.cursor()
 q_existence = "USE openfoodfacts"
 q_creation = "CREATE DATABASE openfoodfacts CHARACTER SET 'utf8' "
 
+query = "DROP DATABASE openfoodfacts"
+mycursor.execute(query)
+
 # check if openfoodfacts database already exists
 try:
     mycursor.execute(q_existence)
     print("\n La base de données |openfoodfacts| existe déjà \n")
-
 # if openfoodfacts database already exists
 except:
     try:
@@ -25,7 +27,7 @@ except:
         mycursor.execute(q_creation)
 # check if openfoodfacts database has been created
         mycursor.execute(q_existence)   
-        print("\n La base de données |openfoodfacts| a bien été créée. \n")
+        print("\n La base de données |openfoodfacts| a bien été créée. \n")    
 # if openfoodfacts database has not been created
     except:
         print("\n La base de données |openfoodfacts| n'a pas pu être créée. \n")
--- tables_creation.py
+++ tables_creation.py
@@ -10,3 +10,56 @@ db = mysql.connector.connect(
 )
 
 mycursor = db.cursor()
+
+query = "CREATE TABLE openfoodfacts.product ("\
+"id INT NOT NULL AUTO_INCREMENT,"\
+"name VARCHAR(120) NOT NULL,"\
+"brand VARCHAR(120) NOT NULL,"\
+"nutrition_grade VARCHAR(1) NOT NULL,"\
+"stores VARCHAR(120) NOT NULL,"\
+"url VARCHAR(255) NOT NULL,"\
+"PRIMARY KEY (id))"\
+"ENGINE = INNODB"
+mycursor.execute(query)
+
+query = "CREATE TABLE openfoodfacts.category ("\
+"id INT NOT NULL AUTO_INCREMENT,"\
+"name VARCHAR(120) NOT NULL,"\
+"PRIMARY KEY (id))"\
+"ENGINE = INNODB"
+mycursor.execute(query)
+
+query = "CREATE TABLE openfoodfacts.product_category ("\
+"id INT NOT NULL AUTO_INCREMENT,"\
+"category_id INT NOT NULL,"\
+"product_id INT NOT NULL,"\
+"PRIMARY KEY (id))"\
+"ENGINE = INNODB"
+mycursor.execute(query)
+
+query = "CREATE TABLE openfoodfacts.favorite ("\
+"id INT NOT NULL AUTO_INCREMENT,"\
+"product_id INT NOT NULL,"\
+"PRIMARY KEY (id))"\
+"ENGINE = INNODB"
+mycursor.execute(query)
+
+
+"""
+foreign keys
+"""
+query = "ALTER TABLE openfoodfacts.product_category "\
+"ADD CONSTRAINT prod_cat_category_fk "\
+"FOREIGN KEY (category_id) REFERENCES openfoodfacts.category(id) ON DELETE CASCADE ON UPDATE CASCADE"
+mycursor.execute(query)
+
+query = "ALTER TABLE openfoodfacts.product_category "\
+"ADD CONSTRAINT prod_cat_product_fk "\
+"FOREIGN KEY (product_id) REFERENCES openfoodfacts.product(id) ON DELETE CASCADE ON UPDATE CASCADE"
+mycursor.execute(query)
+
+query = "ALTER TABLE openfoodfacts.favorite "\
+"ADD UNIQUE KEY product_id_UNIQUE (product_id),"\
+"ADD CONSTRAINT favorite_product_fk FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE"
+mycursor.execute(query)
+
