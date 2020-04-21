c0e37dcf4d7f9cc9ab73a22eda76a06e3b472495
Florian Delage <delageflo@ymail.com>
2020-04-21 18:45:30 +0200

    test user story: creation db

--- run.py
+++ run.py
@@ -7,4 +7,34 @@ class ProgramManager:
     def __init__(self):
         self.db = Database("localhost", "root", "")
 
+    def launch(self):
+        run = True
+        print("\n Bienvenue sur OpenFoodFacts \n")
+        while run:
+            choice = input("1- Déjà utilisateur ? \n"
+                           "2- Nouvel utilisateur ? \n"
+                           "3- Quitter \n"
+                           ">>> Entrez votre choix: ")
+            if choice == "1":
+                db_name = input("\n >>> Entrez le nom de votre base de données: ")
+                db_name = db_name.lower()
+                q = f"USE {db_name}"
+                try:
+                    self.db.mycursor.execute(q)
+                    print(f"\n Bienvenue sur votre base de données |{db_name}| \n")
+                except:
+                    print(f"\n La base de données |{db_name}| n'existe pas \n")
 
+            elif choice == "2":
+                db_name = input("\n >>> Entrer le nom de la base de données que vous souhaitez créer: ")
+                db_name = db_name.lower()
+                self.db.create_db(db_name)
+                self.db.create_tables(db_name)
+
+                print(f"\n Bienvenue sur votre base de données |{db_name}| \n")
+
+
+            elif choice == "3":
+                run = False
+            else:
+                print("\n Aucun choix ne correspond à la commande saisie \n")
