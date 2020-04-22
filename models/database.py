09c3746dc3a3567dbfc156a1314e38a5f68d0082
Florian Delage <delageflo@ymail.com>
2020-04-22 12:39:49 +0200

    user doesn t choose db name

--- models/database.py
+++ models/database.py
@@ -1,6 +1,6 @@
 
 import mysql.connector
-
+import requests
 
 class Database:
     def __init__(self, host, user, passwd):
@@ -16,13 +16,13 @@ class Database:
     
         self.cursor = self.connection.cursor()
 
-    def create_db(self, db_name):
-        existence_query = f"USE {db_name}"
-        creation_query = f"CREATE DATABASE {db_name} CHARACTER SET 'utf8' "
+    def create_db(self):
+        existence_query = "USE openfoodfacts"
+        creation_query = "CREATE DATABASE CHARACTER SET 'utf8' "
         # if openfoodfacts already exists
         try:
             self.cursor.execute(existence_query)
-            print(f"\n La base de données |{db_name}| existe déjà \n")
+            print("\n La base de données |openfoodfacts| existe déjà \n")
         # if openfoodfacts database doesn't exist
         except:
             try:
@@ -30,24 +30,24 @@ class Database:
                 self.cursor.execute(creation_query)
         # check if openfoodfacts database has been created
                 self.cursor.execute(existence_query)   
-                print(f"\n La base de données |{db_name}| a bien été créée \n")    
+                print("\n La base de données |openfoodfacts| a bien été créée \n")    
         # if openfoodfacts database has not been created
             except:
-                print(f"\n La base de données |{db_name}| n'a pas pu être créée \n")
+                print("\n La base de données |openfoodfacts| n'a pas pu être créée \n")
 
-    def drop_db(self, db_name):
-        drop_query = f"DROP DATABASE {db_name}"
+    def drop_db(self):
+        drop_query = "DROP DATABASE openfoodfacts"
         try:
             self.cursor.execute(drop_query)
-            print(f"\n La base de données |{db_name}| a bien été supprimée \n")
+            print("\n La base de données |openfoodfacts| a bien été supprimée \n")
         except:
             mysql.connector.errors.DatabaseError: "1008 (HY000): Can't drop database 'openfoodfacts'; database doesn't exist"
-            print(f"\n La base de données |{db_name}| n'a pas pu être supprimée car elle n'existe pas \n")
+            print("\n La base de données |openfoodfacts| n'a pas pu être supprimée car elle n'existe pas \n")
 
     def create_tables(self, db_name):
         self.connection._database = db_name
 
-        query = f"CREATE TABLE {db_name}.product ("\
+        query = "CREATE TABLE openfoodfacts.product ("\
         "id INT NOT NULL AUTO_INCREMENT,"\
         "name VARCHAR(120) NOT NULL,"\
         "brand VARCHAR(120) NOT NULL,"\
@@ -58,14 +58,14 @@ class Database:
         "ENGINE = INNODB"
         self.cursor.execute(query)
 
-        query = f"CREATE TABLE {db_name}.category ("\
+        query = "CREATE TABLE openfoodfacts.category ("\
         "id INT NOT NULL AUTO_INCREMENT,"\
         "name VARCHAR(120) NOT NULL,"\
         "PRIMARY KEY (id))"\
         "ENGINE = INNODB"
         self.cursor.execute(query)
 
-        query = f"CREATE TABLE {db_name}.product_category ("\
+        query = "CREATE TABLE openfoodfacts.product_category ("\
         "id INT NOT NULL AUTO_INCREMENT,"\
         "category_id INT NOT NULL,"\
         "product_id INT NOT NULL,"\
@@ -73,7 +73,7 @@ class Database:
         "ENGINE = INNODB"
         self.cursor.execute(query)
 
-        query = f"CREATE TABLE {db_name}.favorite ("\
+        query = "CREATE TABLE openfoodfacts.favorite ("\
         "id INT NOT NULL AUTO_INCREMENT,"\
         "product_id INT NOT NULL,"\
         "PRIMARY KEY (id))"\
@@ -83,20 +83,37 @@ class Database:
         """
         foreign keys
         """
-        query = f"ALTER TABLE {db_name}.product_category "\
+        query = "ALTER TABLE openfoodfacts.product_category "\
         "ADD CONSTRAINT prod_cat_category_fk "\
-        f"FOREIGN KEY (category_id) REFERENCES {db_name}.category(id) ON DELETE CASCADE ON UPDATE CASCADE"
+        "FOREIGN KEY (category_id) REFERENCES openfoodfacts.category(id) ON DELETE CASCADE ON UPDATE CASCADE"
         self.cursor.execute(query)
 
-        query = f"ALTER TABLE {db_name}.product_category "\
+        query = "ALTER TABLE openfoodfacts.product_category "\
         "ADD CONSTRAINT prod_cat_product_fk "\
-        f"FOREIGN KEY (product_id) REFERENCES {db_name}.product(id) ON DELETE CASCADE ON UPDATE CASCADE"
+        f"FOREIGN KEY (product_id) REFERENCES openfoodfacts.product(id) ON DELETE CASCADE ON UPDATE CASCADE"
         self.cursor.execute(query)
 
-        query = f"ALTER TABLE {db_name}.favorite "\
+        query = "ALTER TABLE openfoodfacts.favorite "\
         "ADD UNIQUE KEY product_id_UNIQUE (product_id),"\
         "ADD CONSTRAINT favorite_product_fk FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE"
         self.cursor.execute(query)
 
         self.connection.commit()
         self.connection.close()
+
+    def data_insertion(self, db_name):
+        self.connection._database = db_name
+
+        request = requests.get("https://fr.openfoodfacts.org/categorie/pates-a-tartiner-aux-noisettes-et-au-cacao.json")
+        data = request.json()
+
+        products = data["products"]
+
+        for p in products:
+            try:
+                query = "INSERT INTO product (name, brand, nutrition_grade, stores, url) VALUES (%s, %s, %s, %s, %s)"
+                values = (p['product_name'], p['brands'], p['nutrition_grade_fr'], p['stores'], p['url'])
+                self.cursor.execute(query, values)
+                self.connection.commit()
+            except:
+                pass
--- run.py
+++ run.py
@@ -7,34 +7,33 @@ class ProgramManager:
     def __init__(self):
         self.db = Database("localhost", "root", "")
 
-    def launch(self):
+    def run(self):
         run = True
-        print("\n Bienvenue sur OpenFoodFacts \n")
+        print("\n Bienvenue sur OpenFoodFacts !\n")
         while run:
-            choice = input("1- Déjà utilisateur ? \n"
-                           "2- Nouvel utilisateur ? \n"
-                           "3- Quitter \n"
-                           ">>> Entrez votre choix: ")
+            choice = input(" 1- Déjà utilisateur ? \n"
+                           " 2- Nouvel utilisateur ? \n"
+                           " 3- Quitter \n"
+                           "\n Entrez votre choix >>> ")
             if choice == "1":
-                db_name = input("\n >>> Entrez le nom de votre base de données: ")
+                db_name = input("\n Entrez le nom de votre base de données >>> ")
                 db_name = db_name.lower()
                 q = f"USE {db_name}"
                 try:
-                    self.db.mycursor.execute(q)
+                    self.db.cursor.execute(q)
                     print(f"\n Bienvenue sur votre base de données |{db_name}| \n")
                 except:
                     print(f"\n La base de données |{db_name}| n'existe pas \n")
 
             elif choice == "2":
-                db_name = input("\n >>> Entrer le nom de la base de données que vous souhaitez créer: ")
+                db_name = input("\n Entrer le nom de la base de données que vous souhaitez créer >>> ")
                 db_name = db_name.lower()
                 self.db.create_db(db_name)
-                self.db.create_tables(db_name)
-
+                q = f"USE {db_name}"
                 print(f"\n Bienvenue sur votre base de données |{db_name}| \n")
-
 
             elif choice == "3":
                 run = False
+                print(f"\n À bientôt !\n")
             else:
                 print("\n Aucun choix ne correspond à la commande saisie \n")
