f77b783974dd94748150884e13c5a65b58512752
Florian Delage <delageflo@ymail.com>
2021-05-11 15:01:46 +0200

    add docstrings to entityppy

--- app/models/entity.py
+++ app/models/entity.py
@@ -5,6 +5,9 @@ import inspect
 class Entity():
 
     def object_attributes_to_str(self):
+        """
+        returns a string containing all the attributes class
+        """
         obj_columns = ""
         for attribute in self.__dict__.keys():
             if type(self.__dict__[attribute]) is not list:
@@ -12,6 +15,9 @@ class Entity():
         return obj_columns[:-1:]
 
     def object_values_to_str(self):
+        """
+        returns a string containing all the attributes' values of the class
+        """
         obj_values = ""
         for attribute in self.__dict__.keys():
             if type(self.__dict__[attribute]) is not list:
@@ -20,6 +26,10 @@ class Entity():
 
     @classmethod
     def params(cls):
+        """
+        returns a dict containing all the attributes and values of the class
+        if it isnt a list
+        """
         attributes = inspect.getmembers(cls, lambda attr: not(inspect.isroutine(attr)))
         filtered_attributes = dict([attr for attr in attributes if not(attr[0].startswith('__') and attr[0].endswith('__'))])
         return {
