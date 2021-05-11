26da91be16bc586550d137f5e124ff444d48614f
Florian Delage <delageflo@ymail.com>
2021-05-11 14:29:55 +0200

    add docstrings to program_manager.py

--- app/utils/program_manager.py
+++ app/utils/program_manager.py
@@ -10,6 +10,9 @@ class ProgramManager:
         self.controller = Controller()
 
     def run(self):
+        """
+        launches and ensures the correct process of the program
+        """
         self.controller.welcome()
         self.controller.db_management_menu_process()
         self.controller.find_or_display_substitute_process()
